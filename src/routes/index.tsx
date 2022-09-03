65790515488dd46fdb6bfb9ecfde30ae3b683fdb
Guilherme-Boves <ghboves@gmail.com>
2022-09-03 15:22:34 -0300

    Telas de login, dashboard e order completas

---
+++ src/routes/index.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { AuthContext } from "../contexts/AuthContext";
+
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
+
+function Routes(){
+    
+    const { isAuthenticated, loading } = useContext(AuthContext)
+    
+
+
+    if(loading) {
+        return(
+            <View 
+                style={{
+                    flex:1, 
+                    backgroundColor: '#1d1d2e', 
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <ActivityIndicator size={60} color="#F5F7FB" />
+            </View>
+        )
+    }
+    
+    return(
+        isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
+    )
+}
+
+export default Routes;
